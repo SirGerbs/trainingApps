462116cc676046fd9b0b41be7e26f1b54a82f958
kali <gerben_kleijn@hotmail.com>
2021-08-04 11:14:31 -0700

    basic working app.js

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -6,11 +6,12 @@ const ReactDOM = require('react-dom');
 class TestForm extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {
-          investment: '', 
-          years: '', 
-          interest: ''
-        };
+      this.state = { 
+        investment: 0, 
+        years: 0, 
+        interest: 0,        
+        isSubmitted: false
+      };
   
       this.handleInputChange = this.handleInputChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
@@ -25,15 +26,7 @@ class TestForm extends React.Component {
   
     handleSubmit(event) {
       //alert('The following was submitted: ' + this.state.investment + ' ' + this.state.years + ' ' + this.state.interest);
-      fetch('/api/investments', {
-        method: 'POST',
-        // We convert the React state to JSON and send it as the POST body
-        body: JSON.stringify(this.state)
-      }).then(function(response) {
-        console.log(response)
-        return response.json();
-      });
-
+      this.setState({isSubmitted: true});
       event.preventDefault();
     }
 
@@ -44,25 +37,84 @@ class TestForm extends React.Component {
             <form onSubmit={this.handleSubmit}>
             <label>
                 Initial Investment:
-                <input type="text" name="investment" onChange={this.handleInputChange} />
+                <input type="number" name="investment" onChange={this.handleInputChange} />
             </label>
             <br />
             <label>
                 Years:
-                <input type="text" name="years" onChange={this.handleInputChange} />
+                <input type="number" name="years" onChange={this.handleInputChange} />
             </label>
             <br />
             <label>
                 Expected interest per year:
-                <input type="text" name="interest" onChange={this.handleInputChange} />
+                <input type="number" name="interest" onChange={this.handleInputChange} />
             </label>
             <br />
             <input type="submit" value="Submit" />
             </form>
+            {this.state.isSubmitted && <Results investment={this.state.investment} years={this.state.years} interest={this.state.interest}/>}
         </>
       );
     }
   }
+
+class Results extends React.Component {
+  render() {
+    const years = +this.props.years; //convert string to number
+    var investment = +this.props.investment;
+    const interest = 100 + +this.props.interest;
+    var yearsArray = new Array(years + 1);
+    yearsArray[0] = {
+      "year":0,
+      "investment":investment.toFixed(2)
+    }
+    for (var i = 1; i < yearsArray.length; i++){
+      const year = i;
+      investment = (investment * interest) / 100; 
+      yearsArray[i] = {
+        "year":i,
+        "investment":investment.toFixed(2)
+      }
+    }
+
+    console.log(yearsArray);
+
+    const listItems = yearsArray.map(year =>
+      <Year key={year.year} year={year.year} investment={year.investment} />
+      //<li key={year.year.toString()}>{year.year} {year.investment}</li>
+      //<li>test</li>
+    );
+  
+		return (
+      <>
+      <h1>Investment Details</h1>
+      <p>Your initial investment of ${this.props.investment} will grow to ${yearsArray.slice(-1)[0].investment} after {this.props.years} years with a {this.props.interest}% interest rate year over year</p>
+			<table>
+        <tbody>
+          <tr>
+            <th>Year</th>
+            <th>Investment</th>
+          </tr>
+        </tbody>
+        {listItems}
+      </table>
+      </>
+		);
+	}
+}
+
+class Year extends React.Component {
+  render() {
+    return (
+      <tbody>
+        <tr>
+          <td>{this.props.year}</td>
+          <td>${this.props.investment}</td>
+        </tr>
+      </tbody>
+    )
+  }
+}
 
 ReactDOM.render(
 <TestForm />,
